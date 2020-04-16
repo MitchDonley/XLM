d9a9f4b2a5dc8023dee9f50a7a6a6a4a0f66067a
Mitch Donley <mitchdonley@gatech.edu>
2020-04-16 00:53:49 -0400

    added training for xnli

--- prepare-xnli.sh
+++ prepare-xnli.sh
@@ -31,25 +31,25 @@ wget -c https://dl.fbaipublicfiles.com/XLM/codes_xnli_15 -P $MAIN_PATH
 wget -c https://dl.fbaipublicfiles.com/XLM/vocab_xnli_15 -P $MAIN_PATH
 
 
-## Prepare monolingual data
-# apply BPE codes and binarize the monolingual corpora
-for lg in ar bg de el en es fr hi ru sw th tr ur vi zh; do
-    for split in train valid test; do
-    $FASTBPE applybpe $PROCESSED_PATH/$lg.$split $WIKI_PATH/txt/$lg.$split $CODES_PATH
-    python preprocess.py $VOCAB_PATH $PROCESSED_PATH/$lg.$split
-    done
-done
+# ## Prepare monolingual data
+# # apply BPE codes and binarize the monolingual corpora
+# for lg in ar bg de el en es fr hi ru sw th tr ur vi zh; do
+#     for split in train valid test; do
+#     $FASTBPE applybpe $PROCESSED_PATH/$lg.$split $WIKI_PATH/txt/$lg.$split $CODES_PATH
+#     python preprocess.py $VOCAB_PATH $PROCESSED_PATH/$lg.$split
+#     done
+# done
 
-## Prepare parallel data
-# apply BPE codes and binarize the parallel corpora
-for pair in ar-en bg-en de-en el-en en-es en-fr en-hi en-ru en-sw en-th en-tr en-ur en-vi en-zh; do
-    for lg in $(echo $pair | sed -e 's/\-/ /g'); do
-        for split in train valid test; do
-            $FASTBPE applybpe $PROCESSED_PATH/$pair.$lg.$split $PARA_PATH/$pair.$lg.$split $CODES_PATH
-            python preprocess.py $VOCAB_PATH $PROCESSED_PATH/$pair.$lg.$split
-        done
-    done
-done
+# ## Prepare parallel data
+# # apply BPE codes and binarize the parallel corpora
+# for pair in ar-en bg-en de-en el-en en-es en-fr en-hi en-ru en-sw en-th en-tr en-ur en-vi en-zh; do
+#     for lg in $(echo $pair | sed -e 's/\-/ /g'); do
+#         for split in train valid test; do
+#             $FASTBPE applybpe $PROCESSED_PATH/$pair.$lg.$split $PARA_PATH/$pair.$lg.$split $CODES_PATH
+#             python preprocess.py $VOCAB_PATH $PROCESSED_PATH/$pair.$lg.$split
+#         done
+#     done
+# done
 
 ## Prepare XNLI data
 rm -rf $PROCESSED_PATH/eval/XNLI
