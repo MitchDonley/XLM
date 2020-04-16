17bcf5eba3994ac9ffbe5d5cfed459875ae238e0
Mitch Donley <mitchdonley@gatech.edu>
2020-04-16 19:01:57 -0400

    Updated XNLI params

--- train_xnli_standard.sh
+++ train_xnli_standard.sh
@@ -6,10 +6,10 @@ python glue-xnli.py \
 --model_path mlm_tlm_xnli15_1024.pth \
 --data_path "/content/gdrive/My Drive/NLP-project/processed" \
 --transfer_tasks XNLI \
---optimizer_e sgd,lr=0.000025 \
---optimizer_p sgd,lr=0.000025 \
+--optimizer_e sgd,lr=0.000125 \
+--optimizer_p sgd,lr=0.000125 \
 --finetune_layers "0:_1" \
---batch_size 4 \
+--batch_size 8 \
 --n_epochs 250 \
 --epoch_size 20000 \
 --max_len 256 \
