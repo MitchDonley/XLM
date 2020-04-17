9cc15fa114d14d3035d3886cde0b8ffa34a5d666
Mitch Donley <mitchdonley@gatech.edu>
2020-04-17 14:41:36 -0400

    Changed fine tune learning rate

--- train_xnli_standard.sh
+++ train_xnli_standard.sh
@@ -6,9 +6,9 @@ python glue-xnli.py \
 --model_path mlm_tlm_xnli15_1024.pth \
 --data_path "/content/gdrive/My Drive/NLP-project/processed" \
 --transfer_tasks XNLI \
---optimizer_e sgd,lr=0.000125 \
---optimizer_p sgd,lr=0.000125 \
---finetune_layers "0:0" \
+--optimizer_e adam,lr=0.000125 \
+--optimizer_p adam,lr=0.000125 \
+--finetune_layers "0:_1" \
 --batch_size 8 \
 --n_epochs 250 \
 --epoch_size 20000 \
