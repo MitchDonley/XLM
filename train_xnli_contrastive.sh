3841e749a955896918d4a043e8809865356bd3bb
Mitch Donley <mitchdonley@gatech.edu>
2020-04-18 22:28:01 -0400

    Contrastive model on XNLI

---
+++ train_xnli_contrastive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python glue-xnli.py \
+--exp_name test_xnli_mlm_tlm_contrastive \
+--dump_path "/content/gdrive/My Drive/NLP-project/dumped/" \
+--model_path "/content/gdrive/My Drive/NLP-project/dumped/fine_tune_xnli_tlm_contrastive/rva7ib66ir/best-avg_valid_tlm_ppl.pth" \
+--data_path "/content/gdrive/My Drive/NLP-project/processed" \
+--transfer_tasks XNLI \
+--optimizer_e sgd,lr=0.000125 \
+--optimizer_p sgd,lr=0.000125 \
+--finetune_layers "0:_1" \
+--batch_size 8 \
+--n_epochs 250 \
+--epoch_size 20000 \
+--max_len 256 \
+--max_vocab 95000 \
