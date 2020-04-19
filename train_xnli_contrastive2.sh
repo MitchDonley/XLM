bf2ffb17a2f967fcf0dfce03101ab48485ae090d
Mitch Donley <mitchdonley@gatech.edu>
2020-04-18 23:46:33 -0400

    Adding a second training script

---
+++ train_xnli_contrastive2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python glue-xnli.py \
+--exp_name test_xnli_mlm_tlm_contrastive \
+--dump_path "/content/gdrive/My Drive/NLP-project/dumped/" \
+--model_path ./best-avg_valid_tlm_ppl.pth \
+--data_path "/content/gdrive/My Drive/NLP-project/processed" \
+--transfer_tasks XNLI \
+--optimizer_e adam,lr=0.000025 \
+--optimizer_p adam,lr=0.000025 \
+--finetune_layers "0:_1" \
+--batch_size 8 \
+--n_epochs 250 \
+--epoch_size 20000 \
+--max_len 256 \
+--max_vocab 95000 \
