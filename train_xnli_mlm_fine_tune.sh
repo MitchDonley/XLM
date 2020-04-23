e72221822157ddd4a0330ab3b37111575b4b73f1
Mitch Donley <mitchdonley@gatech.edu>
2020-04-22 22:46:30 -0400

    Added mlm and tlm train xnli

---
+++ train_xnli_mlm_fine_tune.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python glue-xnli.py \
+--exp_name test_xnli_mlm_tlm_fine_tune \
+--dump_path "/content/gdrive/My Drive/NLP-project/dumped/" \
+--model_path ./best-avg_valid_tlm_ppl.pth \
+--data_path "/content/gdrive/My Drive/NLP-project/processed" \
+--transfer_tasks XNLI \
+--optimizer_e adam,lr=0.00003 \
+--optimizer_p adam,lr=0.00003 \
+--finetune_layers "0:_1" \
+--batch_size 4 \
+--n_epochs 250 \
+--epoch_size 5000 \
+--max_len 256 \
+--max_vocab 95000 \
